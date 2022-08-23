8da51bf802a7497ece2ccc61b73eb87ec7b285e8
ogbenisamu <samijiyemi@gmail.com>
2022-08-23 22:48:15 +0100

    playground

--- playground/global.js
+++ playground/global.js
@@ -1,9 +1,25 @@
-const sumUpTo = (n) => {
-  let total = 0;
-  for (let i = 1; i < n; i++) {
-    total += 1;
-  }
-  return total;
-};
+const print = console.log;
 
-console.log(sumUpTo(10));
+class Application {
+  constructor(packageName, instance, port) {
+    this.packageName = packageName;
+    this.instance = packageName();
+    this.port = port;
+  }
+
+  startServer() {
+    this.instance.listen(this.port, () => {
+      print(`server runing on port: ${this.port}`);
+    });
+  }
+
+  routeHandling() {
+    this.instance.get("/", (req, res) => {
+      res.json({ name: "Ijiyemi Samuel Write this package!" });
+    });
+  }
+}
+
+const server = new Application(require("express"), this.instance, 3000);
+server.startServer();
+server.routeHandling();
